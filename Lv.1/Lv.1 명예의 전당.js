ba9127b2f93cd28725c2588bcafa75f3d8cc563b
Daen12 <sdy971213@gmail.com>
2023-01-07 20:45:43 +0900

    solved Lv.1 on Jan 7th

---
+++ Lv.1/Lv.1 명예의 전당.js
@@ -0,0 +1,18 @@
+// score 배열에서 0-n번째 어레이를 추출하기
+// 거기서 k개의 가장 큰 수들을 어레이화
+// 거기서 가장 작은 수를 새로운 배열에 매번 append.
+// 그 배열을 반환
+
+function solution(k, score) {
+    const answer = [];
+    for (let i = 1; i <= score.length; i++) {
+        const compare = score.slice(0, i);
+        const highestK = compare.sort((a, b) => b - a).slice(0, k);
+        answer.push(Math.min(...highestK));
+    }
+    return answer;
+}
+
+const score = [0, 300, 40, 300, 20, 70, 150, 50, 500, 1000];
+
+console.log(solution(4, score));
---
+++ Lv.1/memo.js
@@ -0,0 +1,5 @@
+const arr = [5, 2, 3, 4, 1];
+console.log(arr.sort((a, b) => b - a));
+
+const test = [100, 10];
+console.log(Math.min(...test));
